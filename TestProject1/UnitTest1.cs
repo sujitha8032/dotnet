fa52545443c42b906814ccd9e4b5921f4ea446bc
Suman Prasad <sumanprasad9766@gmail.com>
2023-01-08 23:30:57 +0530

    .NET consoleapp2 updates

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+
+public class UnitTest1
+{
+    private const string Expected = "Hello Welcome to the DevOps World everyone!!";
+    [TestMethod]
+    public void TestMethod1()
+    {
+        using (var sw = new StringWriter())
+        {
+            Console.SetOut(sw);
+            ConsoleApp2.Program.Main();
+
+            var result = sw.ToString().Trim();
+            Assert.AreEqual(Expected, result);
+        }
+    }
+}
+
